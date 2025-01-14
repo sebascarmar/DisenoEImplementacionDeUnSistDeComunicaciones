182175ed309b1c0078f0658c3f4754222a0ff0a8
Sebastian Carreño Marin <sebacarmar@gmail.com>
2025-01-14 14:54:08 -0300

    Corrige el nombre de la función

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def r_rcosine(fc, fs, rolloff, nbauds, norm):
     return t_vect_n, y_vect, dot
 
 
-def slicer_qpsk(x):
+def slicer_pam(x):
     if(x>=0):
         a = 1;
     else:
@@ -231,8 +231,8 @@ for i in range(NSYMB*OS_DSP):
     if((i+1)%OS_DSP)==0: # Downsampling to BR rate (os=1)
         k = int(i/OS_DSP)
         # Slicer
-        rx_symI_slcr[k] = slicer_qpsk(rx_symI_fcr[i])
-        rx_symQ_slcr[k] = slicer_qpsk(rx_symQ_fcr[i])
+        rx_symI_slcr[k] = slicer_pam(rx_symI_fcr[i])
+        rx_symQ_slcr[k] = slicer_pam(rx_symQ_fcr[i])
         
         # Error for LMS
         coeff_err_I = ((rx_symI_fcr[i]-rx_symI_slcr[k])*np.cos(nco_out) -
