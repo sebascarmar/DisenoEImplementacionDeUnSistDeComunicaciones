d399ef5288a01329f711f07a31c8693dc7c39817
sebacarmar <sebacarmar@gmail.com>
2025-01-18 23:38:41 -0300

    Crea un archivo con las funciones y lo importa en los scripts

--- float_point/main.py
+++ float_point/main.py
@@ -3,72 +3,8 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.io import savemat
 import math
+import functions as fn
 from classes.prbs9 import prbs9
-
-
-def r_rcosine(fc, fs, rolloff, nbauds, norm):
-
-    rolloff = rolloff + 0.0001
-    BR = fc*2       #Baud Rate
-    Tbaud = 1 / BR       #Time interval between two consecutive symbols
-    Ts = 1 / fs      #Time between 2 consecutive samples at Tx output
-
-    if ((Tbaud/Ts)*nbauds)%2 == 0:
-        t_vect = np.arange(-0.5*nbauds*Tbaud, 0.5*nbauds*Tbaud+Ts, Ts)
-    else:
-        t_vect = np.arange(-0.5*nbauds*Tbaud, 0.5*nbauds*Tbaud, Ts)
-
-    t_vect_n = t_vect / Tbaud # normaliza
-
-    y_vect = []
-    #filtro pasabajos
-    for t in t_vect_n:
-        if (t == 0):
-            y_vect.append((1+rolloff*(4/np.pi-1)))
-        else:
-            y_vect.append((4*rolloff*t*np.cos(((1+rolloff)*np.pi*t))+
-                                       np.sin((1-rolloff)*np.pi*t))/
-                          (np.pi*t*(1-(16*rolloff*rolloff*t*t))))
-
-
-    y_vect = np.array(y_vect)
-    if norm:
-        y_vect = y_vect/np.sum(y_vect)   #normaliza ganancia, 0db
-
-    dot = (np.sum(y_vect ** 2))
-
-    return t_vect_n, y_vect, dot
-
-
-def slicer_pam(x):
-    if(x>=0):
-        a = 1;
-    else:
-        a =-1;
-
-    return a
-
-
-def find_delay(signal1, signal2):
-    # Calculate cross-correlation
-    correlation = np.correlate(signal1, signal2, mode='full')
-    # Find the index of the maximum correlation
-    delay = np.argmax(correlation) - (len(signal2) - 1)
-    return delay
-
-
-def theoric_ber(M, SNRdB):
-    SNR_slicer  = 10**(SNRdB/10)
-    EbNo = SNR_slicer/np.log2(M)
-    k    = np.log2(M)
-    x    = np.sqrt(3*k*EbNo/(M-1))
-    ber  = (4/k)*(1-1/np.sqrt(M))*(1/2)*math.erfc(x/np.sqrt(2))
-
-    return ber
-
-def inv(arreglo):
-    # Retorna el arreglo con sus símbolos invertidos
-    return np.where(arreglo == 1, -1, 1)
 
 
 ####################################################################################
@@ -105,7 +41,7 @@ START_CNT = START_SYN + 4*511*511
 # np.random.seed(2)  # set the seed
 
 
-############################## TRANSCEIVER #############################
+############################## TRANSMITTER  #############################
 
 #### Bits generation
 tx_bitI_prbs = np.zeros(NSYMB)
@@ -128,7 +64,7 @@ tx_symQ_up = np.zeros(OS*NSYMB); tx_symQ_up[0:len(tx_symQ_up):int(OS)]=tx_symQ_m
 
 
 #### RRC Filter
-(t, rrc, dot) = r_rcosine(fc=BR/2, fs=OS*BR, rolloff=BETA, nbauds=NBAUD, norm=True)
+(t, rrc, dot) = fn.r_rcosine(fc=BR/2, fs=OS*BR, rolloff=BETA, nbauds=NBAUD, norm=True)
 
 tx_symI_rrc = signal.lfilter(rrc, [1], tx_symI_up)
 tx_symQ_rrc = signal.lfilter(rrc, [1], tx_symQ_up)
@@ -231,8 +167,8 @@ for i in range(NSYMB*OS_DSP):
     if((i+1)%OS_DSP)==0: # Downsampling to BR rate (os=1)
         k = int(i/OS_DSP)
         # Slicer
-        rx_symI_slcr[k] = slicer_pam(rx_symI_fcr[i])
-        rx_symQ_slcr[k] = slicer_pam(rx_symQ_fcr[i])
+        rx_symI_slcr[k] = fn.slicer_pam(rx_symI_fcr[i])
+        rx_symQ_slcr[k] = fn.slicer_pam(rx_symQ_fcr[i])
         
         # Error for LMS
         coeff_err_I = ((rx_symI_fcr[i]-rx_symI_slcr[k])*np.cos(nco_out) -
@@ -286,14 +222,14 @@ for angle in [0, 90, 180, 270]:
         rx_slcr_I =     rx_symI_slcr[START_SYN+511*511*0:START_SYN+511*511*1]
         rx_slcr_Q =     rx_symQ_slcr[START_SYN+511*511*0:START_SYN+511*511*1]
     elif( angle == 90 ):
-        rx_slcr_I = inv(rx_symQ_slcr[START_SYN+511*511*1:START_SYN+511*511*2])
+        rx_slcr_I = fn.inv(rx_symQ_slcr[START_SYN+511*511*1:START_SYN+511*511*2])
         rx_slcr_Q =     rx_symI_slcr[START_SYN+511*511*1:START_SYN+511*511*2]
     elif( angle == 180 ):
-        rx_slcr_I = inv(rx_symI_slcr[START_SYN+511*511*2:START_SYN+511*511*3])
-        rx_slcr_Q = inv(rx_symQ_slcr[START_SYN+511*511*2:START_SYN+511*511*3])
+        rx_slcr_I = fn.inv(rx_symI_slcr[START_SYN+511*511*2:START_SYN+511*511*3])
+        rx_slcr_Q = fn.inv(rx_symQ_slcr[START_SYN+511*511*2:START_SYN+511*511*3])
     else: # angle==270
         rx_slcr_I =     rx_symQ_slcr[START_SYN+511*511*3:START_SYN+511*511*4]
-        rx_slcr_Q = inv(rx_symI_slcr[START_SYN+511*511*3:START_SYN+511*511*4])
+        rx_slcr_Q = fn.inv(rx_symI_slcr[START_SYN+511*511*3:START_SYN+511*511*4])
 
     # Symbol error counting for every angle
     min_error_aux = len(rx_prbs_I)
@@ -337,14 +273,14 @@ if( rot_ang_detec == 0 ):
     rx_slcr_I =     rx_symI_slcr
     rx_slcr_Q =     rx_symQ_slcr
 elif( rot_ang_detec == 90 ):
-    rx_slcr_I = inv(rx_symQ_slcr)
+    rx_slcr_I = fn.inv(rx_symQ_slcr)
     rx_slcr_Q =     rx_symI_slcr
 elif( rot_ang_detec == 180 ):
-    rx_slcr_I = inv(rx_symI_slcr)
-    rx_slcr_Q = inv(rx_symQ_slcr)
+    rx_slcr_I = fn.inv(rx_symI_slcr)
+    rx_slcr_Q = fn.inv(rx_symQ_slcr)
 else: # rot_ang_detec=270
     rx_slcr_I =     rx_symQ_slcr
-    rx_slcr_Q = inv(rx_symI_slcr)
+    rx_slcr_Q = fn.inv(rx_symI_slcr)
 
 # BER (Lane I)
 bit_err_I = 0
@@ -363,7 +299,7 @@ for i in range(START_CNT,len(rx_slcr_Q)-latency):
     bit_tot_Q += 1
 
 
-th_ber = theoric_ber(M, SNR_db)
+th_ber = fn.theoric_ber(M, SNR_db)
 
 print("SNR=", SNR_db, " | f_off=",f_offset)
 print("BER_I: ", bit_err_I/bit_tot_I)
--- float_point/main_ber_sweep.py
+++ float_point/main_ber_sweep.py
@@ -3,72 +3,8 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.io import savemat
 import math
+import functions as fn
 from classes.prbs9 import prbs9
-
-
-def r_rcosine(fc, fs, rolloff, nbauds, norm):
-
-    rolloff = rolloff + 0.0001
-    BR = fc*2       #Baud Rate
-    Tbaud = 1 / BR       #Time interval between two consecutive symbols
-    Ts = 1 / fs      #Time between 2 consecutive samples at Tx output
-
-    if ((Tbaud/Ts)*nbauds)%2 == 0:
-        t_vect = np.arange(-0.5*nbauds*Tbaud, 0.5*nbauds*Tbaud+Ts, Ts)
-    else:
-        t_vect = np.arange(-0.5*nbauds*Tbaud, 0.5*nbauds*Tbaud, Ts)
-
-    t_vect_n = t_vect / Tbaud # normaliza
-
-    y_vect = []
-    #filtro pasabajos
-    for t in t_vect_n:
-        if (t == 0):
-            y_vect.append((1+rolloff*(4/np.pi-1)))
-        else:
-            y_vect.append((4*rolloff*t*np.cos(((1+rolloff)*np.pi*t))+
-                                       np.sin((1-rolloff)*np.pi*t))/
-                          (np.pi*t*(1-(16*rolloff*rolloff*t*t))))
-
-
-    y_vect = np.array(y_vect)
-    if norm:
-        y_vect = y_vect/np.sum(y_vect)   #normaliza ganancia, 0db
-
-    dot = (np.sum(y_vect ** 2))
-
-    return t_vect_n, y_vect, dot
-
-
-def slicer_pam(x):
-    if(x>=0):
-        a = 1;
-    else:
-        a =-1;
-
-    return a
-
-
-def find_delay(signal1, signal2):
-    # Calculate cross-correlation
-    correlation = np.correlate(signal1, signal2, mode='full')
-    # Find the index of the maximum correlation
-    delay = np.argmax(correlation) - (len(signal2) - 1)
-    return delay
-
-
-def theoric_ber(M, SNRdB):
-    SNR_slicer  = 10**(SNRdB/10)
-    EbNo = SNR_slicer/np.log2(M)
-    k    = np.log2(M)
-    x    = np.sqrt(3*k*EbNo/(M-1))
-    ber  = (4/k)*(1-1/np.sqrt(M))*(1/2)*math.erfc(x/np.sqrt(2))
-
-    return ber
-
-def inv(arreglo):
-    # Retorna el arreglo con sus símbolos invertidos
-    return np.where(arreglo == 1, -1, 1)
 
 
 ####################################################################################
@@ -132,7 +68,7 @@ for SNR_db in range(2, SWEEP_TIMES+2):
 
 
     #### RRC Filter
-    (t, rrc, dot) = r_rcosine(fc=BR/2, fs=OS*BR, rolloff=BETA, nbauds=NBAUD, norm=True)
+    (t, rrc, dot) = fn.r_rcosine(fc=BR/2, fs=OS*BR, rolloff=BETA, nbauds=NBAUD, norm=True)
 
     tx_symI_rrc = signal.lfilter(rrc, [1], tx_symI_up)
     tx_symQ_rrc = signal.lfilter(rrc, [1], tx_symQ_up)
@@ -222,7 +158,7 @@ for SNR_db in range(2, SWEEP_TIMES+2):
         fseI_buffer[0]  = rx_symI_agc[i]
         fseQ_buffer[1:] = fseQ_buffer[:-1]
         fseQ_buffer[0]  = rx_symQ_agc[i]
-
+        
         # Filter output
         rx_symI_fse[i] = np.dot(fseI_buffer,fseI_coeff)-np.dot(fseQ_buffer,fseQ_coeff)
         rx_symQ_fse[i] = np.dot(fseI_buffer,fseQ_coeff)+np.dot(fseQ_buffer,fseI_coeff)
@@ -230,13 +166,13 @@ for SNR_db in range(2, SWEEP_TIMES+2):
         # FCR output: multiplication by e^{-jnco_out}
         rx_symI_fcr[i] = rx_symI_fse[i]*np.cos(-nco_out) - rx_symQ_fse[i]*np.sin(-nco_out)
         rx_symQ_fcr[i] = rx_symI_fse[i]*np.sin(-nco_out) + rx_symQ_fse[i]*np.cos(-nco_out)
-
-
+        
+        
         if((i+1)%OS_DSP)==0: # Downsampling to BR rate (os=1)
             k = int(i/OS_DSP)
             # Slicer
-            rx_symI_slcr[k] = slicer_pam(rx_symI_fcr[i])
-            rx_symQ_slcr[k] = slicer_pam(rx_symQ_fcr[i])
+            rx_symI_slcr[k] = fn.slicer_pam(rx_symI_fcr[i])
+            rx_symQ_slcr[k] = fn.slicer_pam(rx_symQ_fcr[i])
             
             # Error for LMS
             coeff_err_I = ((rx_symI_fcr[i]-rx_symI_slcr[k])*np.cos(nco_out) -
@@ -290,19 +226,19 @@ for SNR_db in range(2, SWEEP_TIMES+2):
             rx_slcr_I =     rx_symI_slcr[START_SYN+511*511*0:START_SYN+511*511*1]
             rx_slcr_Q =     rx_symQ_slcr[START_SYN+511*511*0:START_SYN+511*511*1]
         elif( angle == 90 ):
-            rx_slcr_I = inv(rx_symQ_slcr[START_SYN+511*511*1:START_SYN+511*511*2])
+            rx_slcr_I = fn.inv(rx_symQ_slcr[START_SYN+511*511*1:START_SYN+511*511*2])
             rx_slcr_Q =     rx_symI_slcr[START_SYN+511*511*1:START_SYN+511*511*2]
         elif( angle == 180 ):
-            rx_slcr_I = inv(rx_symI_slcr[START_SYN+511*511*2:START_SYN+511*511*3])
-            rx_slcr_Q = inv(rx_symQ_slcr[START_SYN+511*511*2:START_SYN+511*511*3])
+            rx_slcr_I = fn.inv(rx_symI_slcr[START_SYN+511*511*2:START_SYN+511*511*3])
+            rx_slcr_Q = fn.inv(rx_symQ_slcr[START_SYN+511*511*2:START_SYN+511*511*3])
         else: # angle==270
             rx_slcr_I =     rx_symQ_slcr[START_SYN+511*511*3:START_SYN+511*511*4]
-            rx_slcr_Q = inv(rx_symI_slcr[START_SYN+511*511*3:START_SYN+511*511*4])
-
+            rx_slcr_Q = fn.inv(rx_symI_slcr[START_SYN+511*511*3:START_SYN+511*511*4])
+        
         # Symbol error counting for every angle
         min_error_aux = len(rx_prbs_I)
         for BER_IDX in range(len(rx_prbs_I)):
-
+         
             err_sym_count = 0
             # Count errors for each BER_IDX
             for i in range(len(rx_prbs_I)):
@@ -311,20 +247,20 @@ for SNR_db in range(2, SWEEP_TIMES+2):
                     rx_prbs_Q = np.roll(rx_prbs_Q,-1)
                 new_bit_prbs_I = rx_prbs_I[BER_IDX]
                 new_bit_prbs_Q = rx_prbs_Q[BER_IDX]
-
+                
                 if( (new_bit_prbs_I != rx_slcr_I[i+511*BER_IDX]) or (new_bit_prbs_Q != rx_slcr_Q[i+511*BER_IDX]) ):
                     err_sym_count += 1
                 else:
                     err_sym_count = err_sym_count
-
+            
             rx_prbs_I = np.roll(rx_prbs_I,-1)
             rx_prbs_Q = np.roll(rx_prbs_Q,-1)
             
             if( err_sym_count < min_error_aux ):
                 min_error_aux = err_sym_count
                 lat_aux       = BER_IDX
-
-
+        
+        
         if( min_error_aux < min_error ):
             min_error     = min_error_aux
             latency       = lat_aux 
@@ -341,14 +277,14 @@ for SNR_db in range(2, SWEEP_TIMES+2):
         rx_slcr_I =     rx_symI_slcr
         rx_slcr_Q =     rx_symQ_slcr
     elif( rot_ang_detec == 90 ):
-        rx_slcr_I = inv(rx_symQ_slcr)
+        rx_slcr_I = fn.inv(rx_symQ_slcr)
         rx_slcr_Q =     rx_symI_slcr
     elif( rot_ang_detec == 180 ):
-        rx_slcr_I = inv(rx_symI_slcr)
-        rx_slcr_Q = inv(rx_symQ_slcr)
+        rx_slcr_I = fn.inv(rx_symI_slcr)
+        rx_slcr_Q = fn.inv(rx_symQ_slcr)
     else: # rot_ang_detec=270
         rx_slcr_I =     rx_symQ_slcr
-        rx_slcr_Q = inv(rx_symI_slcr)
+        rx_slcr_Q = fn.inv(rx_symI_slcr)
 
     # BER (Lane I)
     bit_err_I = 0
@@ -368,7 +304,7 @@ for SNR_db in range(2, SWEEP_TIMES+2):
 
     bersI_contadas[SNR_db] = bit_err_I/bit_tot_I
     bersQ_contadas[SNR_db] = bit_err_Q/bit_tot_Q
-    th_ber[SNR_db] = theoric_ber(M, SNR_db)
+    th_ber[SNR_db] = fn.theoric_ber(M, SNR_db)
 
     print("SNR=", SNR_db, " | f_off=",f_offset)
     print("BER_I: ", bit_err_I/bit_tot_I)
