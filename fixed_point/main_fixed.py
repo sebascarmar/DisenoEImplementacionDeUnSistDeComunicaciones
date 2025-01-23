d1b346b9f579344bd0b3e1a137da5aba76dd50a9
sebacarmar <sebacarmar@gmail.com>
2025-01-23 01:16:57 -0300

    Actualiza el log de datos para post process

--- fixed_point/main_fixed.py
+++ fixed_point/main_fixed.py
@@ -51,60 +51,60 @@ np.random.seed(2)  # set the seed
 ############################## TRANSMITTER  #############################
 
 #### Bits generation
-tx_bitI_prbs_log = np.zeros(NSYMB)
-tx_bitQ_prbs_log = np.zeros(NSYMB)
+TX_BITI_PRBS_LOG = np.zeros(NSYMB)
+TX_BITQ_PRBS_LOG = np.zeros(NSYMB)
 
 prbs9I = prbs9(SEED_I)
 prbs9Q = prbs9(SEED_Q)
 
 #### Mapper
-tx_symI_map_log = np.zeros(NSYMB)
-tx_symQ_map_log = np.zeros(NSYMB)
+TX_SYMI_MAP_LOG = np.zeros(NSYMB)
+TX_SYMQ_MAP_LOG = np.zeros(NSYMB)
 
 #### RRC Filter (also up-sampler)
 (t, rrc, dot) = fn.r_rcosine(fc=BR/2, fs=OS*BR, rolloff=BETA, nbauds=NBAUD, norm=True)
 tx_filter_I = poly_filter(rrc, NBAUD, OS, NSYMB)
 tx_filter_Q = poly_filter(rrc, NBAUD, OS, NSYMB)
-tx_symI_rrc_log = np.zeros(OS*NSYMB)
-tx_symQ_rrc_log = np.zeros(OS*NSYMB)
+TX_SYMI_RRC_LOG = np.zeros(OS*NSYMB)
+TX_SYMQ_RRC_LOG = np.zeros(OS*NSYMB)
 
 ################################ CHANNEL ###############################
 #### AWGN
 awgn_gen_I = noise_gen(OS, SNR_db)
 awgn_gen_Q = noise_gen(OS, SNR_db)
-ch_symI_noisy_log = np.zeros(OS*NSYMB)
-ch_symQ_noisy_log = np.zeros(OS*NSYMB)
+CH_SYMI_NOISY_LOG = np.zeros(OS*NSYMB)
+CH_SYMQ_NOISY_LOG = np.zeros(OS*NSYMB)
 
 #### Frequency Offset
 offset_freq = offset_gen(OS*BR, f_offset)
-ch_symI_rot_log = np.zeros(OS*NSYMB)
-ch_symQ_rot_log = np.zeros(OS*NSYMB)
+CH_SYMI_ROT_LOG = np.zeros(OS*NSYMB)
+CH_SYMQ_ROT_LOG = np.zeros(OS*NSYMB)
 
 #### Channel filter
 ch_filt_coeff   = signal.firwin(numtaps=17, cutoff=fc_ch_filter ,window='hamming', fs=4*BR)
 chann_filt_I = fir_filter(ch_filt_coeff)
 chann_filt_Q = fir_filter(ch_filt_coeff)
-ch_symI_chfilt_log = np.zeros(OS*NSYMB)
-ch_symQ_chfilt_log = np.zeros(OS*NSYMB)
+CH_SYMI_CHFILT_LOG = np.zeros(OS*NSYMB)
+CH_SYMQ_CHFILT_LOG = np.zeros(OS*NSYMB)
 
 ############################### RECEIVER ###############################
 #### Anti-alias filter
 aaf_coeff  = signal.firwin(numtaps=17, cutoff=BR ,window='hamming', fs=4*BR)
 aaf_filt_I = fir_filter(aaf_coeff)
 aaf_filt_Q = fir_filter(aaf_coeff)
-rx_symI_aaf_log = np.zeros(OS*NSYMB)
-rx_symQ_aaf_log = np.zeros(OS*NSYMB)
+RX_SYMI_AAF_LOG = np.zeros(OS*NSYMB)
+RX_SYMQ_AAF_LOG = np.zeros(OS*NSYMB)
 
 #### Downsampler (rate 2)
 dw_aaf_shifter_I = np.zeros(OS_DSP)
 dw_aaf_shifter_Q = np.zeros(OS_DSP)
-rx_symI_dw_log = np.zeros(NSYMB*OS_DSP)
-rx_symQ_dw_log = np.zeros(NSYMB*OS_DSP)
+RX_SYMI_DW_RATE2_LOG = np.zeros(NSYMB*OS_DSP)
+RX_SYMQ_DW_RATE2_LOG = np.zeros(NSYMB*OS_DSP)
 
 #### AGC
 agc_gain    = 1
-rx_symI_agc_log =  np.zeros(NSYMB*OS_DSP)
-rx_symQ_agc_log =  np.zeros(NSYMB*OS_DSP)
+RX_SYMI_AGC_LOG =  np.zeros(NSYMB*OS_DSP)
+RX_SYMQ_AGC_LOG =  np.zeros(NSYMB*OS_DSP)
 
 #### DSP
 # FSE variables
@@ -113,23 +113,35 @@ fseQ_buffer = np.zeros(NTAPS_FSE)
 fseI_coeff  = np.zeros(NTAPS_FSE); fseI_coeff[int(NTAPS_FSE/2)] = 1
 fseQ_coeff  = np.zeros(NTAPS_FSE); fseQ_coeff[int(NTAPS_FSE/2)] = 0
 
-rx_symI_fse = np.zeros(NSYMB*OS_DSP)
-rx_symQ_fse = np.zeros(NSYMB*OS_DSP)
-log_step        = 500
-coeffs_log      = np.zeros((NTAPS_FSE, int(NSYMB/log_step)))
+rx_symI_fse = 0.0
+rx_symQ_fse = 0.0
+RX_SYMI_FSE_LOG = np.zeros(NSYMB*OS_DSP)
+RX_SYMQ_FSE_LOG = np.zeros(NSYMB*OS_DSP)
+
+log_step     = 500
+FSE_I_COEFFS_LOG = np.zeros((NTAPS_FSE, int(NSYMB/log_step)))
 
 # FCR variables
 nco_out     = 0
 int_err     = 0 
-rx_symI_fcr = np.zeros(NSYMB*OS_DSP)
-rx_symQ_fcr = np.zeros(NSYMB*OS_DSP)
-nco_log = np.zeros(NSYMB*OS_DSP)
-int_log = np.zeros(NSYMB*OS_DSP)
+rx_symI_fcr = 0.0
+rx_symQ_fcr = 0.0
+RX_SYMI_FCR_LOG = np.zeros(NSYMB*OS_DSP)
+RX_SYMQ_FCR_LOG = np.zeros(NSYMB*OS_DSP)
+NCO_OUT_LOG     = np.zeros(NSYMB*OS_DSP)
+INT_ERR_LOG     = np.zeros(NSYMB*OS_DSP)
 
+# Downsampler (rate 1)
+RX_SYMI_DW_RATE1_LOG = np.zeros(NSYMB)
+RX_SYMQ_DW_RATE1_LOG = np.zeros(NSYMB)
 
-# Slicer variables
-rx_symI_slcr = np.zeros(NSYMB)
-rx_symQ_slcr = np.zeros(NSYMB)
+# Slicer variables     np.zeros(NSYMB
+RX_SYMI_SLCR_LOG = np.zeros(NSYMB)
+RX_SYMQ_SLCR_LOG = np.zeros(NSYMB)
+
+#### Demapper
+RX_BITI_DEMAP_LOG= np.zeros(NSYMB)
+RX_BITQ_DEMAP_LOG= np.zeros(NSYMB)
 
 ############################ BIT-ERROR RATE ############################
 ber_IjQ = ber(SEED_I, SEED_Q, START_SYN, START_CNT)
@@ -145,48 +157,48 @@ for i in range(NSYMB*OS):
         #### Bits generation
         tx_bitI_prbs = prbs9I.get_new_bit()
         tx_bitQ_prbs = prbs9Q.get_new_bit()
-        #tx_bitI_prbs_log[n] = tx_bitI_prbs
-        #tx_bitQ_prbs_log[n] = tx_bitQ_prbs
+        TX_BITI_PRBS_LOG[n] = tx_bitI_prbs
+        TX_BITQ_PRBS_LOG[n] = tx_bitQ_prbs
         
         #### Mapper
         tx_symI_map = 1 if(tx_bitI_prbs == 0) else -1
         tx_symQ_map = 1 if(tx_bitQ_prbs == 0) else -1
-        #tx_symI_map_log[n]=tx_symI_map
-        #tx_symQ_map_log[n]=tx_symQ_map
+        TX_SYMI_MAP_LOG[n]=tx_symI_map
+        TX_SYMQ_MAP_LOG[n]=tx_symQ_map
 
     #### Up-sampler & Tx Filter
     tx_symI_rrc = tx_filter_I.convol(i, tx_symI_map) 
     tx_symQ_rrc = tx_filter_Q.convol(i, tx_symQ_map) 
-    #tx_symI_rrc_log[i] = tx_symI_rrc
-    #tx_symQ_rrc_log[i] = tx_symQ_rrc
+    TX_SYMI_RRC_LOG[i] = tx_symI_rrc
+    TX_SYMQ_RRC_LOG[i] = tx_symQ_rrc
 
     ################################ CHANNEL ###############################
     #### Noise
     ch_symI_noisy = awgn_gen_I.add_noise(tx_symI_rrc)
     ch_symQ_noisy = awgn_gen_Q.add_noise(tx_symQ_rrc)
-    #ch_symI_noisy_log[i] = ch_symI_noisy
-    #ch_symQ_noisy_log[i] = ch_symQ_noisy
+    CH_SYMI_NOISY_LOG[i] = ch_symI_noisy
+    CH_SYMQ_NOISY_LOG[i] = ch_symQ_noisy
 
     #### Frequency Offset
     if( i>(NSYMB_CONVERGENCE*OS-1) ):
         ch_symI_rot, ch_symQ_rot = offset_freq.get_offset(ch_symI_noisy, ch_symQ_noisy)
     else:
         ch_symI_rot, ch_symQ_rot = offset_freq.get_no_offset(ch_symI_noisy, ch_symQ_noisy)
-    ch_symI_rot_log[i] = ch_symI_rot
-    ch_symQ_rot_log[i] = ch_symQ_rot
+    CH_SYMI_ROT_LOG[i] = ch_symI_rot
+    CH_SYMQ_ROT_LOG[i] = ch_symQ_rot
 
     #### Channel filter (ISI)
     ch_symI_chfilt = chann_filt_I.convol(ch_symI_rot)
     ch_symQ_chfilt = chann_filt_Q.convol(ch_symQ_rot)
-    ch_symI_chfilt_log[i] = ch_symI_chfilt
-    ch_symQ_chfilt_log[i] = ch_symQ_chfilt
+    CH_SYMI_CHFILT_LOG[i] = ch_symI_chfilt
+    CH_SYMQ_CHFILT_LOG[i] = ch_symQ_chfilt
 
     ############################### RECEIVER ###############################
     #### Anti-alias filter
     rx_symI_aaf = aaf_filt_I.convol(ch_symI_chfilt)
     rx_symQ_aaf = aaf_filt_Q.convol(ch_symQ_chfilt)
-    #rx_symI_aaf_log[i] = rx_symI_aaf
-    #rx_symQ_aaf_log[i] = rx_symQ_aaf
+    RX_SYMI_AAF_LOG[i] = rx_symI_aaf
+    RX_SYMQ_AAF_LOG[i] = rx_symQ_aaf
 
     #### Downsamplers (rate 2)
     dw_aaf_shifter_I    = np.roll(dw_aaf_shifter_I,1)
@@ -199,14 +211,14 @@ for i in range(NSYMB*OS):
 
     if( i%OS_DSP ==0 ): # Downsampling to OS_DSP*BR rate
         j = int(i/OS_DSP)
-        #rx_symI_dw_log[j] = rx_symI_dw
-        #rx_symQ_dw_log[j] = rx_symQ_dw
+        RX_SYMI_DW_RATE2_LOG[j] = rx_symI_dw
+        RX_SYMQ_DW_RATE2_LOG[j] = rx_symQ_dw
         
         #### AGC
         rx_symI_agc =  rx_symI_dw * agc_gain
         rx_symQ_agc =  rx_symQ_dw * agc_gain
-        rx_symI_agc_log[j] = rx_symI_agc
-        rx_symQ_agc_log[j] = rx_symQ_agc
+        RX_SYMI_AGC_LOG[j] = rx_symI_agc
+        RX_SYMQ_AGC_LOG[j] = rx_symQ_agc
         
         #### DSP
         # Filter buffer
@@ -216,36 +228,44 @@ for i in range(NSYMB*OS):
         fseQ_buffer[0]  = rx_symQ_agc
         
         # Filter output
-        rx_symI_fse[j] = np.dot(fseI_buffer,fseI_coeff)-np.dot(fseQ_buffer,fseQ_coeff)
-        rx_symQ_fse[j] = np.dot(fseI_buffer,fseQ_coeff)+np.dot(fseQ_buffer,fseI_coeff)
+        rx_symI_fse = np.dot(fseI_buffer,fseI_coeff)-np.dot(fseQ_buffer,fseQ_coeff)
+        rx_symQ_fse = np.dot(fseI_buffer,fseQ_coeff)+np.dot(fseQ_buffer,fseI_coeff)
+        RX_SYMI_FSE_LOG[j] = rx_symI_fse 
+        RX_SYMQ_FSE_LOG[j] = rx_symQ_fse 
         
         # FCR output: multiplication by e^{-jnco_out}
-        rx_symI_fcr[j] = rx_symI_fse[j]*np.cos(-nco_out) - rx_symQ_fse[j]*np.sin(-nco_out)
-        rx_symQ_fcr[j] = rx_symI_fse[j]*np.sin(-nco_out) + rx_symQ_fse[j]*np.cos(-nco_out)
-        #nco_log[j] = nco_out
-        #int_log[j] = int_err
+        rx_symI_fcr = rx_symI_fse*np.cos(-nco_out) - rx_symQ_fse*np.sin(-nco_out)
+        rx_symQ_fcr = rx_symI_fse*np.sin(-nco_out) + rx_symQ_fse*np.cos(-nco_out)
+        RX_SYMI_FCR_LOG[j] = rx_symI_fcr 
+        RX_SYMQ_FCR_LOG[j] = rx_symQ_fcr 
+        NCO_OUT_LOG[j]     = nco_out
+        INT_ERR_LOG[j]     = int_err
         
         if((j+1)%OS_DSP)==0: # Downsampling to BR rate (os=1)
             k = int(j/OS_DSP)
+            RX_SYMI_DW_RATE1_LOG[k] = rx_symI_fcr
+            RX_SYMQ_DW_RATE1_LOG[k] = rx_symQ_fcr
             # Slicer
-            rx_symI_slcr[k] = fn.slicer_pam(rx_symI_fcr[j])
-            rx_symQ_slcr[k] = fn.slicer_pam(rx_symQ_fcr[j])
+            rx_symI_slcr = fn.slicer_pam(rx_symI_fcr)
+            rx_symQ_slcr = fn.slicer_pam(rx_symQ_fcr)
+            RX_SYMI_SLCR_LOG[k] = rx_symI_slcr
+            RX_SYMQ_SLCR_LOG[k] = rx_symQ_slcr
             
             # Error for LMS
-            coeff_err_I = ((rx_symI_fcr[j]-rx_symI_slcr[k])*np.cos(nco_out) -
-                           (rx_symQ_fcr[j]-rx_symQ_slcr[k])*np.sin(nco_out))
-            coeff_err_Q = ((rx_symI_fcr[j]-rx_symI_slcr[k])*np.sin(nco_out) +
-                           (rx_symQ_fcr[j]-rx_symQ_slcr[k])*np.cos(nco_out))
+            coeff_err_I = ((rx_symI_fcr-rx_symI_slcr)*np.cos(nco_out) -
+                           (rx_symQ_fcr-rx_symQ_slcr)*np.sin(nco_out))
+            coeff_err_Q = ((rx_symI_fcr-rx_symI_slcr)*np.sin(nco_out) +
+                           (rx_symQ_fcr-rx_symQ_slcr)*np.cos(nco_out))
             
             fseI_coeff = (fseI_coeff*(1-lms_step*lms_leak) - 
                            lms_step*(coeff_err_I*fseI_buffer + coeff_err_Q*fseQ_buffer))
             fseQ_coeff = (fseQ_coeff*(1-lms_step*lms_leak) +
                            lms_step*( coeff_err_I*fseQ_buffer - coeff_err_Q*fseI_buffer))
             if( (((j+1)/OS_DSP)%log_step) == 0 ):
-                coeffs_log[:, int(((j+1)/OS_DSP)/log_step)-1] = fseI_coeff
+                FSE_I_COEFFS_LOG[:, int(((j+1)/OS_DSP)/log_step)-1] = fseI_coeff
             
             # Phase error
-            prod = (rx_symI_fcr[j]+1j*rx_symQ_fcr[j])*(rx_symI_slcr[k]-1j*rx_symQ_slcr[k])
+            prod = (rx_symI_fcr+1j*rx_symQ_fcr)*(rx_symI_slcr-1j*rx_symQ_slcr)
             if( np.abs(prod)!= 0 ):
                 prod_norm = prod/np.abs(prod)
             else:
@@ -261,8 +281,10 @@ for i in range(NSYMB*OS):
             nco_out  = (prop_err+int_err) + nco_out
             
             # Demapper
-            rx_bitI_demap = 0 if(rx_symI_slcr[k]==1) else 1
-            rx_bitQ_demap = 0 if(rx_symQ_slcr[k]==1) else 1
+            rx_bitI_demap = 0 if(rx_symI_slcr==1) else 1
+            rx_bitQ_demap = 0 if(rx_symQ_slcr==1) else 1
+            RX_BITI_DEMAP_LOG[k] = rx_bitI_demap
+            RX_BITQ_DEMAP_LOG[k] = rx_bitQ_demap
          
             ######################## BIT-ERROR RATE ########################
             if( k<START_SYN ): ### Wait for convergence before start synchronization
@@ -282,39 +304,45 @@ print("SNR=", SNR_db, " | f_off=",f_offset, " | step=", lms_step, " | Kp=", Kp, 
 print("BER_I: ", rate_I)
 print("BER_Q: ", rate_I)
 print("theo_ber: ", th_ber)
-# Guardar el array en un archivo de texto
-#np.savetxt('tx_symI_map.txt', tx_symI_map_log, delimiter=',')
-#np.savetxt('tx_symQ_map.txt', tx_symQ_map_log, delimiter=',')
-#np.savetxt('tx_symI_rrc.txt', tx_symI_rrc_log, delimiter=',')
-#np.savetxt('tx_symQ_rrc.txt', tx_symQ_rrc_log, delimiter=',')
-#np.savetxt('ch_symI_noisy.txt', ch_symI_noisy_log, delimiter=',')
-#np.savetxt('ch_symQ_noisy.txt', ch_symQ_noisy_log, delimiter=',')
-#np.savetxt('ch_symI_rot.txt', ch_symI_rot_log, delimiter=',')
-#np.savetxt('ch_symQ_rot.txt', ch_symQ_rot_log, delimiter=',')
-#np.savetxt('ch_symI_chfilt.txt', ch_symI_chfilt_log, delimiter=',')
-#np.savetxt('ch_symQ_chfilt.txt', ch_symQ_chfilt_log, delimiter=',')
-#np.savetxt('rx_symI_aaf.txt', rx_symI_aaf_log, delimiter=',')
-#np.savetxt('rx_symQ_aaf.txt', rx_symQ_aaf_log, delimiter=',')
-#np.savetxt('rx_symI_dw.txt', rx_symI_dw_log, delimiter=',')
-#np.savetxt('rx_symQ_dw.txt', rx_symQ_dw_log, delimiter=',')
-#np.savetxt('rx_symI_agc.txt', rx_symI_agc_log, delimiter=',')
-#np.savetxt('rx_symQ_agc.txt', rx_symQ_agc_log, delimiter=',')
-#np.savetxt('rx_symI_fse.txt', rx_symI_fse, delimiter=',')
-#np.savetxt('rx_symQ_fse.txt', rx_symQ_fse, delimiter=',')
-#np.savetxt('rx_symI_fcr.txt', rx_symI_fcr, delimiter=',')
-#np.savetxt('rx_symQ_fcr.txt', rx_symQ_fcr, delimiter=',')
-np.savetxt('rx_symI_slcr.txt', rx_symI_slcr, delimiter=',')
-np.savetxt('rx_symQ_slcr.txt', rx_symQ_slcr, delimiter=',')
-
-#-------------------------------------------
+SIMULATION_DATA = [latency, rot_ang_detec, SNR_db, f_offset, lms_step, Kp, fc_ch_filter, rate_I, rate_Q, th_ber]
 
 
-
-
-
-
-
-
+################################## LOGS ################################
+np.savetxt('./logs/simulation_data.txt' , SIMULATION_DATA      , delimiter=',')
+np.savetxt('./logs/tx_bitI_prbs.txt'    , TX_BITI_PRBS_LOG     , delimiter=',')
+np.savetxt('./logs/tx_bitQ_prbs.txt'    , TX_BITQ_PRBS_LOG     , delimiter=',')
+np.savetxt('./logs/tx_symI_map.txt'     , TX_SYMI_MAP_LOG      , delimiter=',')
+np.savetxt('./logs/tx_symQ_map.txt'     , TX_SYMQ_MAP_LOG      , delimiter=',')
+np.savetxt('./logs/coeffs_rrc.txt'      , rrc                  , delimiter=',')
+np.savetxt('./logs/tx_symI_rrc.txt'     , TX_SYMI_RRC_LOG      , delimiter=',')
+np.savetxt('./logs/tx_symQ_rrc.txt'     , TX_SYMQ_RRC_LOG      , delimiter=',')
+np.savetxt('./logs/ch_symI_noisy.txt'   , CH_SYMI_NOISY_LOG    , delimiter=',')
+np.savetxt('./logs/ch_symQ_noisy.txt'   , CH_SYMQ_NOISY_LOG    , delimiter=',')
+np.savetxt('./logs/ch_symI_rot.txt'     , CH_SYMI_ROT_LOG      , delimiter=',')
+np.savetxt('./logs/ch_symQ_rot.txt'     , CH_SYMQ_ROT_LOG      , delimiter=',')
+np.savetxt('./logs/coeffs_chfilt.txt'   , ch_filt_coeff        , delimiter=',')
+np.savetxt('./logs/ch_symI_chfilt.txt'  , CH_SYMI_CHFILT_LOG   , delimiter=',')
+np.savetxt('./logs/ch_symQ_chfilt.txt'  , CH_SYMQ_CHFILT_LOG   , delimiter=',')
+np.savetxt('./logs/coeffs_aafilt.txt'   , aaf_coeff            , delimiter=',')
+np.savetxt('./logs/rx_symI_aaf.txt'     , RX_SYMI_AAF_LOG      , delimiter=',')
+np.savetxt('./logs/rx_symQ_aaf.txt'     , RX_SYMQ_AAF_LOG      , delimiter=',')
+np.savetxt('./logs/rx_symI_dw_rate2.txt', RX_SYMI_DW_RATE2_LOG , delimiter=',')
+np.savetxt('./logs/rx_symQ_dw_rate2.txt', RX_SYMQ_DW_RATE2_LOG , delimiter=',')
+np.savetxt('./logs/rx_symI_agc.txt'     , RX_SYMI_AGC_LOG      , delimiter=',')
+np.savetxt('./logs/rx_symQ_agc.txt'     , RX_SYMQ_AGC_LOG      , delimiter=',')
+np.savetxt('./logs/coeffs_fse_I.txt'    , FSE_I_COEFFS_LOG     , delimiter=',')
+np.savetxt('./logs/rx_symI_fse.txt'     , RX_SYMI_FSE_LOG      , delimiter=',')
+np.savetxt('./logs/rx_symQ_fse.txt'     , RX_SYMQ_FSE_LOG      , delimiter=',')
+np.savetxt('./logs/rx_symI_fcr.txt'     , RX_SYMI_FCR_LOG      , delimiter=',')
+np.savetxt('./logs/rx_symQ_fcr.txt'     , RX_SYMQ_FCR_LOG      , delimiter=',')
+np.savetxt('./logs/nco_out.txt'         , NCO_OUT_LOG          , delimiter=',')
+np.savetxt('./logs/int_error.txt'       , INT_ERR_LOG          , delimiter=',')
+np.savetxt('./logs/rx_symI_dw_rate1.txt', RX_SYMI_DW_RATE1_LOG , delimiter=',')
+np.savetxt('./logs/rx_symQ_dw_rate1.txt', RX_SYMQ_DW_RATE1_LOG , delimiter=',')
+np.savetxt('./logs/rx_symI_slcr.txt'    , RX_SYMI_SLCR_LOG     , delimiter=',')
+np.savetxt('./logs/rx_symQ_slcr.txt'    , RX_SYMQ_SLCR_LOG     , delimiter=',')
+np.savetxt('./logs/rx_bitI_demap.txt'   , RX_BITI_DEMAP_LOG    , delimiter=',')
+np.savetxt('./logs/rx_bitQ_demap.txt'   , RX_BITQ_DEMAP_LOG    , delimiter=',')
 
 
 
