6202ef08b2f538f8c17b21ad582db70bdb119439
sebacarmar <sebacarmar@gmail.com>
2025-01-23 21:22:14 -0300

    Agrega caso en el que f_offset=0

--- fixed_point/classes/offset_gen.py
+++ fixed_point/classes/offset_gen.py
@@ -3,15 +3,25 @@ import numpy as np
 class offset_gen:
 
     def __init__(self, fs, f_offset):
-        time_vec = np.arange(0, 1/f_offset, 1/fs)
-        titas    = np.array(2*np.pi*f_offset * time_vec, dtype=np.float32)
-        
-        self.sin = np.sin(titas)
-        self.cos = np.cos(titas)
-        self.idx = len(self.sin)-1
-        
-        if (fs%f_offset != 0) or ((fs/f_offset)%2 != 0):
-            print("OFFSET_GEN CLASS: f_offset value is not an even multiple of fs")
+        if( f_offset > 0.0 ):
+            time_vec = np.arange(0, 1/f_offset, 1/fs)
+            titas    = np.array(2*np.pi*f_offset * time_vec, dtype=np.float32)
+            
+            self.sin = np.sin(titas)
+            self.cos = np.cos(titas)
+            self.idx = len(self.sin)-1
+         
+            if (fs%f_offset != 0) or ((fs/f_offset)%2 != 0):
+                print("OFFSET_GEN CLASS: f_offset value is not an even multiple of fs")
+         
+        elif( f_offset == 0.0 ):
+            self.sin = np.full(1000, 1.0)
+            self.cos = np.full(1000, 0.0)
+            self.idx = len(self.sin)-1
+            
+        else:  # f_offset<0
+            print("OFFSET_GEN CLASS: f_offset value is negative")
+            exit(1)
 
     def get_offset(self, symbI_in, symbQ_in):
         
