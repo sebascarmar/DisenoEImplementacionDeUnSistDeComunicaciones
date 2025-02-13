e85642422d4daf5c2256082b924212e2a4b9130a
sebacarmar <sebacarmar@gmail.com>
2025-02-13 01:48:19 -0300

    Agrega lógica de hardware para seno y coseno
    El seno y coseno son necesarios para aplicar la rotación a la
    constelación, separando las operaciones por lane: por un lado la parte
    real (I), y por otro la parte imaginaria (Q).
    Estas funciones son implementadas mediante una tabla de 1/4 de seno, que
    para determinado número de muestras se tendrá determinadas frecuencias
    de acuerdo al paso con el cual se recorra la tabla.
    Para el caso elegido (4097 muestras), solamente se tienen las opciones
    de 12.2k, 24.4k y 48.8k, ya que frecuencias superiores no son seguidas
    por el FCR.

--- fixed_point/classes/offset_gen.py
+++ fixed_point/classes/offset_gen.py
@@ -1,44 +1,76 @@
 import numpy as np
 
 class offset_gen:
+# Given a clock frequency of fs=100 MHz:
+#           2^(step-1)*[fs/(2048*4)]
+#   -for step=1   =>  f_offset = 12.2 kHz
+#   -for step=2   =>  f_offset = 24.4 kHz
+#   -for step=4   =>  f_offset = 48.8 kHz
 
-    def __init__(self, fs, f_offset):
-        if( f_offset > 0.0 ):
-            time_vec = np.arange(0, 1/f_offset, 1/fs)
-            titas    = np.array(2*np.pi*f_offset * time_vec, dtype=np.float32)
-            
-            self.sin = np.sin(titas)
-            self.cos = np.cos(titas)
-            self.idx = len(self.sin)-1
-         
-            if (fs%f_offset != 0) or ((fs/f_offset)%2 != 0):
-                print("OFFSET_GEN CLASS: f_offset value is not an even multiple of fs")
-         
-        elif( f_offset == 0.0 ):
-            self.sin = np.full(1000, 1.0)
-            self.cos = np.full(1000, 0.0)
-            self.idx = len(self.sin)-1
-            
-        else:  # f_offset<0
-            print("OFFSET_GEN CLASS: f_offset value is negative")
+    def __init__(self, f_offset):
+        
+        self.nsamp      = 2049
+        wt              = np.linspace(0, np.pi/2, self.nsamp)  
+        self.sin_val    = np.sin(wt)
+        
+        if( f_offset == 12e3 ):
+            self.step = 1
+        elif( f_offset == 24e3 ):
+            self.step = 2
+        elif( f_offset == 48e3 ):
+            self.step = 4
+        else:
+            print("OFFSET_GEN CLASS: invalid valur of f_offset")
             exit(1)
+        
+        self.idx  = 4*(self.nsamp-self.step)
+
+
+    def __sin(self):
+        
+        # Get the value from the table depending on the corresponding quarter 
+        if(self.idx<=(self.nsamp-1)):
+            sin_out = self.sin_val[self.idx]
+        elif(self.idx<=2*(self.nsamp-1)):
+            sin_out = self.sin_val[2*(self.nsamp-1)-self.idx]
+        elif(self.idx<=3*(self.nsamp-1)):
+            sin_out = -self.sin_val[self.idx-2*(self.nsamp-1)]
+        else:
+            sin_out = -self.sin_val[4*(self.nsamp-1)-self.idx]
+        
+        return sin_out
+
+
+    def __cos(self):
+        
+        # Get the value from the table depending on the corresponding quarter 
+        if(self.idx<=(self.nsamp-1)):
+            cos_out = self.sin_val[(self.nsamp-1)-self.idx]
+        elif(self.idx<=2*(self.nsamp-1)):
+            cos_out = -self.sin_val[self.idx-(self.nsamp-1)]
+        elif(self.idx<=3*(self.nsamp-1)):
+            cos_out = -self.sin_val[3*(self.nsamp-1)-self.idx]
+        else:
+            cos_out = self.sin_val[self.idx-3*(self.nsamp-1)]
+        
+        return cos_out
+
 
     def get_offset(self, symbI_in, symbQ_in):
         
-        if( self.idx==(len(self.sin)-1) ):
-            self.idx = 0
-        else:
-            self.idx += 1
+        # Increment the counter used to index into the sine values table
+        self.idx = (self.idx+self.step) & (4*(self.nsamp-1)-1)
         
-        symI_rot = symbI_in*self.cos[self.idx] - symbQ_in*self.sin[self.idx]
-        symQ_rot = symbI_in*self.sin[self.idx] + symbQ_in*self.cos[self.idx]
+        # Apply rotation to the symbol, with real and imaginary parts handled separately
+        symI_rot = symbI_in*self.__cos() - symbQ_in*self.__sin()
+        symQ_rot = symbI_in*self.__sin() + symbQ_in*self.__cos()
         
         return symI_rot, symQ_rot
 
 
     def get_no_offset(self, symbI_in, symbQ_in):
         
-        symI_rot = symbI_in*self.cos[0] - symbQ_in*self.sin[0]
-        symQ_rot = symbI_in*self.sin[0] + symbQ_in*self.cos[0]
+        symI_norot = symbI_in*1 - symbQ_in*0
+        symQ_norot = symbI_in*0 + symbQ_in*1
         
-        return symI_rot, symQ_rot
+        return symI_norot, symQ_norot
