817f91fa44ff386859f4ca55c840a6f91805a8ba
sebacarmar <sebacarmar@gmail.com>
2025-03-20 18:38:57 -0300

    Agrega señal de control para tasa 2 (50MHz)

--- rtl/control.v
+++ rtl/control.v
@@ -2,48 +2,49 @@
 
 
 module control (
-    output [1:0] o_count    ,
-    output       o_count_max,
+    output [1:0] o_counter          ,
+    output       o_count_max        ,
+    output       o_count_half_or_max,
     
-    input        i_reset    , 
+    input        i_reset            , 
     input        clk       
 );
   
   // Internal registers
-  reg [1:0] counter;
-  reg       max    ;    
+  reg [1:0] r_counter          ;
+  reg       r_count_max        ;    
+  reg       r_count_half_or_max;    
 
   // Counter
   always @(posedge clk) begin: control
     if (i_reset==1'b1) begin
-        counter <= 2'b10;
-        max     <= 1'b0;
+        r_counter           <= 2'b10;
+        r_count_max         <= 1'b0;
+        r_count_half_or_max <= 1'b0;
     end
     else begin
         // Counter increments until 2'b11, then resets to 2'b00 for polyphase filter control
-        if (counter<2'b11) begin
-            counter <= counter + 2'b01;
+        if (r_counter<2'b11) begin
+            r_counter <= r_counter + 2'b01;
         end
         else begin
-            counter <= 2'b00;
+            r_counter <= 2'b00;
         end
        
-        // max is HIGH when counter is 2'b11 due to register delay.
-        if (counter==2'b10) begin
-            max     <= 1'b1;
-        end
-        else begin
-            max     <= 1'b0;
-        end
+        // r_count_max is HIGH when r_counter is 2'b11 due to register delay.
+        r_count_max <= (r_counter==2'b10) ? 1'b1 : 1'b0;
+       
+        // count_half_or_max is HIGH when count is 2'b01 or 2'b11
+        r_count_half_or_max <= (r_counter==2'b01 || r_counter==2'b11) ? 1'b1 : 1'b0;
        
     end
   end
   
   
   // Output asignation
-  assign o_count_max = max    ;
-  assign o_count     = counter;
-
+  assign o_counter           = r_counter;
+  assign o_count_max         = r_count_max    ;
+  assign o_count_half_or_max = r_count_half_or_max;
 
  endmodule
 
