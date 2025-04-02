a0067f451f62eef183550ecde5c2c1f293bb2125
sebacarmar <sebacarmar@gmail.com>
2025-04-02 17:47:49 -0300

    Agrega top con todo el sist. com. con su tb

---
+++ rtl/tmp_top_for_test_bench/scripts_py/graphics_from_verilog.py
@@ -0,0 +1,302 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from scipy import signal
+
+### Calculate the theoric values of BER in function of SNR
+def theoric_ber(SNRdB):
+    M = 4
+    SNR_slicer  = 10**(SNRdB/10)
+    EbNo = SNR_slicer/np.log2(M)
+    k    = np.log2(M)
+    x    = np.sqrt(3*k*EbNo/(M-1))
+    ber  = (4/k)*(1-1/np.sqrt(M))*(1/2)*math.erfc(x/np.sqrt(2))
+
+    return ber
+
+
+
+####################################################################################
+#                                      MAIN                                        #
+####################################################################################
+
+sim_data     = np.loadtxt("file_sim_data.txt", dtype=np.intp)
+
+NSYMB          = sim_data[ 0]
+sigma_int      = sim_data[ 1]
+NBF_SIGMA      = sim_data[ 2]
+NBF_AAFILT_OUT = sim_data[ 3]
+lms_step_int   = sim_data[ 4]
+NBF_LMS_STEP   = sim_data[ 5]
+lms_leak_int   = sim_data[ 6]
+NBF_LMS_LEAK   = sim_data[ 7]
+NTAPS_FSE      = sim_data[ 8]
+NBT_FSE_TAPS   = sim_data[ 9]
+NBF_FSE_TAPS   = sim_data[10]
+NBF_FSE_OUT    = sim_data[11]
+log_step       = sim_data[12]
+latency        = sim_data[13]
+bit_err_I      = sim_data[14]
+bit_err_Q      = sim_data[15]
+bit_tot        = sim_data[16]
+
+
+sigma    = sigma_int / (2**NBF_SIGMA)
+lms_step = lms_step_int / (2**NBF_LMS_STEP)
+lms_leak = lms_leak_int / (2**NBF_LMS_LEAK)
+
+
+OS = 4
+var_signal = 0.1251046972473807 # data taken from float-point simulator
+SNR_slicer  = (var_signal / (2*(sigma**2))) * OS
+SNR_dB      = 10*np.log10(SNR_slicer)
+
+print( "NSYMB =", NSYMB, " | step=", lms_step, " | leak=", lms_leak)
+print( "SNR = {:.3f} dB".format(SNR_dB)," | FSE_TAPS: S({},{}) ".format(NBT_FSE_TAPS, NBF_FSE_TAPS))
+print( "latency:",latency)
+print( "BER_I: {:.3f} %".format((bit_err_I/bit_tot)*100) )
+print( "BER_Q: {:.3f} %".format((bit_err_Q/bit_tot)*100) )
+print( "theo_ber: {:.3f} %".format(theoric_ber(SNR_dB)*100) )
+
+
+
+#### Downsampler (rate 2)
+rx_symI_dw_r2_from_ver = np.loadtxt("file_rx_symI_dwr2.txt", dtype=np.intp)
+rx_symQ_dw_r2_from_ver = np.loadtxt("file_rx_symQ_dwr2.txt", dtype=np.intp)
+
+rx_symI_dw_r2 = rx_symI_dw_r2_from_ver / (2**NBF_AAFILT_OUT)
+rx_symQ_dw_r2 = rx_symQ_dw_r2_from_ver / (2**NBF_AAFILT_OUT)
+
+
+#### FSE Taps
+fse_coeff_I = np.zeros((NTAPS_FSE, int(NSYMB/log_step)))
+fse_coeff_Q = np.zeros((NTAPS_FSE, int(NSYMB/log_step)))
+
+fseI_taps_from_ver = np.loadtxt("file_fse_taps_I.txt", dtype=np.intp)
+fseQ_taps_from_ver = np.loadtxt("file_fse_taps_Q.txt", dtype=np.intp)
+
+NBF_FSE_TAPS = 25
+fseI_taps = fseI_taps_from_ver/(2**NBF_FSE_TAPS)
+fseQ_taps = fseQ_taps_from_ver/(2**NBF_FSE_TAPS)
+for i in range(NTAPS_FSE):
+    for j in range((int(NSYMB/log_step))):
+        fse_coeff_I[i][j] = fseI_taps[i + j*NTAPS_FSE]
+        fse_coeff_Q[i][j] = fseQ_taps[i + j*NTAPS_FSE]
+
+
+#### FSE Out
+rx_symI_fse_from_ver = np.loadtxt("file_rx_symI_fse.txt", dtype=np.intp)
+rx_symQ_fse_from_ver = np.loadtxt("file_rx_symQ_fse.txt", dtype=np.intp)
+
+rx_symI_fse = rx_symI_fse_from_ver / (2**NBF_FSE_OUT)
+rx_symQ_fse = rx_symQ_fse_from_ver / (2**NBF_FSE_OUT)
+
+
+#### Downsampler (rate 1)
+rx_symI_dw_r1_from_ver = np.loadtxt("file_rx_symI_dwr1.txt", dtype=np.intp)
+rx_symQ_dw_r1_from_ver = np.loadtxt("file_rx_symQ_dwr1.txt", dtype=np.intp)
+
+rx_symI_dw_r1 = rx_symI_dw_r1_from_ver / (2**NBF_FSE_OUT)
+rx_symQ_dw_r1 = rx_symQ_dw_r1_from_ver / (2**NBF_FSE_OUT)
+
+
+# Slicer variables
+rx_symI_slcr_from_ver = np.loadtxt("file_rx_symI_slcr.txt", dtype=np.intp)
+rx_symQ_slcr_from_ver = np.loadtxt("file_rx_symQ_slcr.txt", dtype=np.intp)
+
+rx_symI_slcr = rx_symI_slcr_from_ver / (2**NBF_FSE_OUT)
+rx_symQ_slcr = rx_symQ_slcr_from_ver / (2**NBF_FSE_OUT)
+
+
+# Error for LMS
+rx_err_lms_I_from_ver = np.loadtxt("file_rx_error_I.txt", dtype=np.intp)
+rx_err_lms_Q_from_ver = np.loadtxt("file_rx_error_Q.txt", dtype=np.intp)
+
+rx_err_lms_I = rx_err_lms_I_from_ver / (2**NBF_FSE_OUT)
+rx_err_lms_Q = rx_err_lms_Q_from_ver / (2**NBF_FSE_OUT)
+
+
+
+#####################################################################################
+##                                      PLOTS                                       #
+#####################################################################################
+
+############################ PRE-FSE ###############################
+# Group of constellations: Downsampled to rate 2 and FSE output
+plt.figure(figsize=[8,4])
+plt.suptitle('Constellation Diagrams: Downsampled to rate 1 and FSE Output')
+plt.subplot(1,2,1) # after downsampling to rate 2
+plt.plot(rx_symI_dw_r2[len(rx_symI_dw_r2)-8000:],
+         rx_symQ_dw_r2[len(rx_symQ_dw_r2)-8000:],
+         color='chocolate', marker='.', linestyle='',
+         label='dowsamp. (rate 2)')
+plt.xlim((-2, 2))
+plt.ylim((-2, 2))
+plt.axis('equal')
+plt.grid(True)
+plt.ylabel('Imag (Q)')
+plt.xlabel('Real (I)')
+plt.legend(loc="upper left")
+#-------------------------------------------------------
+plt.subplot(1,2,2) # FSE Output
+plt.plot(rx_symI_fse[len(rx_symI_fse)-8000:],
+         rx_symQ_fse[len(rx_symQ_fse)-8000:],
+         color='salmon', marker='.', linestyle='',
+         label="FSE Output")
+plt.xlim((-2, 2))
+plt.ylim((-2, 2))
+plt.axis('equal')
+plt.grid(True)
+plt.ylabel('Imag (Q)')
+plt.xlabel('Real (I)')
+plt.legend(loc="upper left")
+#plt.show()
+
+
+# Downsamp. symbols at rate 1 and FSE Output vs. Time
+plt.figure(figsize=[10,6])
+plt.suptitle('Downsampled symbs (rate 2) and FSE Output')
+plt.subplot(2,1,1)
+plt.plot(rx_symI_dw_r2 ,
+         color='chocolate', marker='.', linestyle='',
+         label="Dws - rate 2")
+plt.ylabel('Real (I)')
+plt.grid(True)
+plt.legend(loc="upper left")
+plt.subplot(2,1,2) # FSE Output
+plt.plot(rx_symI_fse,
+         color='salmon', marker='.', linestyle='',
+         label="FSE Output")
+plt.grid(True)
+plt.ylabel('Real (I)')
+plt.xlabel('Time [n]')
+plt.legend(loc="upper left")
+plt.show()
+
+
+########################### POST-FSE ###############################
+
+# Group of constellations: Downsampled to rate 1, and Sliced
+plt.figure(figsize=[8,4])
+plt.suptitle('Constellation Diagrams: Downsampled to rate 2 and FSE Output')
+plt.subplot(1,2,1) # after downsampling to rate 1
+plt.plot(rx_symI_dw_r1[len(rx_symI_dw_r1)-4000:],
+         rx_symQ_dw_r1[len(rx_symQ_dw_r1)-4000:],
+         color='seagreen', marker='.', linestyle='',
+         label='dowsamp. (rate 1)')
+plt.xlim((-2, 2))
+plt.ylim((-2, 2))
+plt.axis('equal')
+plt.grid(True)
+plt.ylabel('Imag (Q)')
+plt.xlabel('Real (I)')
+plt.legend(loc="upper left")
+#-------------------------------------------------------
+plt.subplot(1,2,2) # after the slicer
+plt.plot(rx_symI_slcr[len(rx_symI_slcr)-4000:],
+         rx_symQ_slcr[len(rx_symQ_slcr)-4000:],
+         color='magenta', marker='.', linestyle='',
+         label="sliced")
+plt.xlim((-2, 2))
+plt.ylim((-2, 2))
+#plt.axis('equal')
+plt.grid(True)
+plt.xlabel('Real (I)')
+plt.legend(loc="upper left")
+plt.show()
+
+
+# Group of data vs time: Downsampled to rate 1 and Sliced
+plt.figure(figsize=[12,6])
+plt.suptitle('Downsampled to rate 1 and Sliced vs time')
+plt.subplot(2,1,1) # after downsampling to rate 1
+plt.plot(rx_symI_dw_r1,
+        color='seagreen', marker='.', linestyle='',
+        label='dowsamp. (rate 1)')
+plt.ylabel('Real (I)')
+plt.grid(True)
+plt.legend(loc="upper left")
+#-------------------------------------------------------
+plt.subplot(2,1,2) # after the slicer
+plt.plot(rx_symI_slcr,
+        color='magenta', marker='.', linestyle='',
+        label="sliced")
+plt.grid(True)
+plt.ylabel('Real (I)')
+plt.xlabel('Time [n]')
+plt.legend(loc="upper left")
+# plt.show()
+
+
+
+##################### Adapatvie Filter graphics ####################
+
+# Get frequencies and magnitudes
+last_fse_taps = fse_coeff_I[:NTAPS_FSE, (int(NSYMB/log_step))-1]
+
+f_fse, h_fse = signal.freqz(last_fse_taps, worN=800, fs=50e6)
+# Find the -3 dB point
+fc_idx = np.where(20*np.log10(np.abs(h_fse)) <= (20*np.log10(np.abs(h_fse[50])) - 3.01))[0][0]
+actual_fc_fse = f_fse[fc_idx]
+
+## Frequency response of the channel filter
+plt.figure(figsize=(8, 5))
+plt.plot(f_fse, 20*np.log10(np.abs(h_fse)), color='saddlebrown')
+plt.axhline(y=20*np.log10(np.abs(h_fse[50]))-3.01,
+            color='black',linestyle='dashed',linewidth=2.0,
+            label=f"{20*np.log10(np.abs(h_fse[50]))-3.01:.2f}dB")
+plt.axvline(x=actual_fc_fse,color='gray',linewidth=2.0,
+            label=f"{actual_fc_fse / 1e6:.2f}MHz")
+plt.axvline(x=12.5e6,color='coral',linewidth=2.0,
+            label=f"{12.5e6 / 1e6:.2f}MHz")
+plt.title("Bode - FSE I")
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("Magnitud [dB]")
+plt.legend(loc="lower left")
+plt.grid(True)
+#plt.show()
+
+
+## Time axis (centered around zero)
+t = np.linspace(-0.5*(1/(50e6))*(len(last_fse_taps)-1),
+                0.5*(1/(50e6))*(len(last_fse_taps)-1),
+                len(last_fse_taps))
+# Impulse response of the transmitter filter
+plt.figure(figsize=[7,4])
+plt.plot(t, last_fse_taps, color='saddlebrown', marker='o',
+         linestyle='-', linewidth=2.0)
+plt.axvline(0, color='k', linestyle='--', linewidth=1.5) 
+plt.title('FSE Coefficients')
+plt.xlabel('Sample')
+plt.ylabel('Magnitud')
+plt.grid(True)
+plt.show()
+
+
+
+## Evolution of FSE coeffcients over time
+plt.figure(figsize=[10,6])
+plt.plot(fse_coeff_I.T)
+plt.title('FSE coefficients (I lane) - decimated')
+plt.grid(True)
+
+plt.figure(figsize=[10,6])
+plt.plot(fse_coeff_Q.T)
+plt.title('FSE coefficients (Q lane) - decimated')
+plt.grid(True)
+plt.show()
+
+
+## Evolution error for LMS over time
+plt.figure(figsize=[10,6])
+plt.plot(rx_err_lms_I)
+plt.title('LMS-Error - Lane I')
+plt.grid(True)
+
+plt.figure(figsize=[10,6])
+plt.plot(rx_err_lms_Q)
+plt.title('LMS-Error - Lane Q')
+plt.grid(True)
+plt.show()
+
+
---
+++ rtl/tmp_top_for_test_bench/tmp_top_for_test_bench.v
@@ -0,0 +1,443 @@
+//============================================================
+// SNR to Variance table in S(8,7) format
+// -----------------------------------------------------------
+// SNR (dB)     Variance σ²     σ² (hex)    S(8,7) format
+// -----------------------------------------------------------
+//       7         0.21875         8'h1c         00011100
+//       8       0.1953125         8'h19         00011001
+//       9        0.171875         8'h16         00010110
+//      10         0.15625         8'h14         00010100
+//      11        0.140625         8'h12         00010010
+//      12           0.125         8'h10         00010000
+// -----------------------------------------------------------
+//============================================================
+`timescale 1ns/1ps
+
+
+// PRBS
+`define PRBS_SEED_I     9'h1AA
+`define PRBS_SEED_Q     9'h1FE
+
+// TRANSMITTER FILTER
+`define TXFILT_COEFF_FILE "./../../../../../../../fixed_point/logs/coeffs_txf.dat"
+`define NBT_TXFILT_COEF   8
+`define NBF_TXFILT_COEF   7
+`define NBT_TXFILT_OUT    8
+`define NBF_TXFILT_OUT    7
+`define NBAUD             5+1
+`define OVERSAMP          4
+
+// NOISE
+`define NOISE_SEED1_I 64'd5030521883283424767 
+`define NOISE_SEED2_I 64'd18445829279364155008 
+`define NOISE_SEED3_I 64'd18436106298727503359 
+`define NOISE_SEED1_Q 64'd14533118196545751551 
+`define NOISE_SEED2_Q 64'd18444914485018758400 
+`define NOISE_SEED3_Q 64'd18425749998705519615 
+`define SIGMA         8'sh1c                 
+`define NBT_SIGMA     8 
+`define NBF_SIGMA     7 
+`define NBT_NOISE     8
+`define NBF_NOISE     7 
+`define NBT_NOISY_SYM 8
+`define NBF_NOISY_SYM 7
+
+// CHANNEL FILTER
+`define NUM_CHFILT_COEF  17
+`define CHFILT_COEFF_FILE "./../../../../../../../fixed_point/logs/coeffs_chfilt.dat"
+`define NBT_CHFILT_COEF   8
+`define NBF_CHFILT_COEF   7
+`define NBT_CHFILT_OUT    8
+`define NBF_CHFILT_OUT    7
+
+// ANTI-ALIAS FILTER
+`define NUM_AAFILT_COEF  17
+`define AAFILT_COEFF_FILE "./../../../../../../../fixed_point/logs/coeffs_aafilt.dat"
+`define NBT_AAFILT_COEF   8
+`define NBF_AAFILT_COEF   7
+`define NBT_AAFILT_OUT    8
+`define NBF_AAFILT_OUT    7
+
+//FSE & LMS
+`define LMS_STEP     12'sh001
+`define LMS_LEAK     11'sh001
+`define NBT_LMS_STEP 12
+`define NBF_LMS_STEP 11
+`define NBT_LMS_LEAK 11
+`define NBF_LMS_LEAK 10
+//----
+`define NUM_FSE_TAPS  9
+`define NBT_FSE_TAPS 28
+`define NBF_FSE_TAPS 25
+`define NBT_FSE_OUT  12
+`define NBF_FSE_OUT   9
+`define NBT_TAPS_ERR 12
+`define NBF_TAPS_ERR  9
+
+// BER
+`define PRBS_MAX_CYCLES  511                
+`define START_SYN        PRBS_MAX_CYCLES*690
+`define START_CNT        START_SYN + 511*511
+
+
+
+module tmp_top_for_test_bench
+#(
+  parameter PRBS_SEED_I       = `PRBS_SEED_I      ,
+  parameter PRBS_SEED_Q       = `PRBS_SEED_Q      ,
+  parameter NBAUD             = `NBAUD            ,
+  parameter OVERSAMP          = `OVERSAMP         ,
+  parameter TXFILT_COEFF_FILE = `TXFILT_COEFF_FILE,
+  parameter NBT_TXFILT_COEF   = `NBT_TXFILT_COEF  ,
+  parameter NBF_TXFILT_COEF   = `NBF_TXFILT_COEF  ,
+  parameter NBT_TXFILT_OUT    = `NBT_TXFILT_OUT   ,
+  parameter NBF_TXFILT_OUT    = `NBF_TXFILT_OUT   ,
+  parameter NOISE_SEED1_I     = `NOISE_SEED1_I    ,
+  parameter NOISE_SEED2_I     = `NOISE_SEED2_I    ,
+  parameter NOISE_SEED3_I     = `NOISE_SEED3_I    ,
+  parameter NOISE_SEED1_Q     = `NOISE_SEED1_Q    ,
+  parameter NOISE_SEED2_Q     = `NOISE_SEED2_Q    ,
+  parameter NOISE_SEED3_Q     = `NOISE_SEED3_Q    ,
+  parameter SIGMA             = `SIGMA            ,
+  parameter NBT_SIGMA         = `NBT_SIGMA        , 
+  parameter NBF_SIGMA         = `NBF_SIGMA        , 
+  parameter NBT_NOISE         = `NBT_NOISE        , 
+  parameter NBF_NOISE         = `NBF_NOISE        ,
+  parameter NBT_NOISY_SYM     = `NBT_NOISY_SYM    ,
+  parameter NBF_NOISY_SYM     = `NBF_NOISY_SYM    ,
+  parameter NUM_CHFILT_COEF   = `NUM_CHFILT_COEF  , 
+  parameter CHFILT_COEFF_FILE = `CHFILT_COEFF_FILE, 
+  parameter NBT_CHFILT_COEF   = `NBT_CHFILT_COEF  , 
+  parameter NBF_CHFILT_COEF   = `NBF_CHFILT_COEF  , 
+  parameter NBT_CHFILT_OUT    = `NBT_CHFILT_OUT   , 
+  parameter NBF_CHFILT_OUT    = `NBF_CHFILT_OUT   , 
+  parameter NUM_AAFILT_COEF   = `NUM_AAFILT_COEF  ,
+  parameter AAFILT_COEFF_FILE = `AAFILT_COEFF_FILE,
+  parameter NBT_AAFILT_COEF   = `NBT_AAFILT_COEF  ,
+  parameter NBF_AAFILT_COEF   = `NBF_AAFILT_COEF  ,
+  parameter NBT_AAFILT_OUT    = `NBT_AAFILT_OUT   ,
+  parameter NBF_AAFILT_OUT    = `NBF_AAFILT_OUT   ,
+  parameter LMS_STEP          = `LMS_STEP         ,
+  parameter LMS_LEAK          = `LMS_LEAK         ,
+  parameter NBT_LMS_STEP      = `NBT_LMS_STEP     ,
+  parameter NBF_LMS_STEP      = `NBF_LMS_STEP     ,
+  parameter NBT_LMS_LEAK      = `NBT_LMS_LEAK     ,
+  parameter NBF_LMS_LEAK      = `NBF_LMS_LEAK     ,
+  parameter NUM_FSE_TAPS      = `NUM_FSE_TAPS     ,
+  parameter NBT_FSE_TAPS      = `NBT_FSE_TAPS     ,
+  parameter NBF_FSE_TAPS      = `NBF_FSE_TAPS     ,
+  parameter NBT_FSE_OUT       = `NBT_FSE_OUT      ,
+  parameter NBF_FSE_OUT       = `NBF_FSE_OUT      ,
+  parameter NBT_TAPS_ERR      = `NBT_TAPS_ERR     ,
+  parameter NBF_TAPS_ERR      = `NBF_TAPS_ERR     ,
+  parameter PRBS_MAX_CYCLES   = `PRBS_MAX_CYCLES  ,
+  parameter START_SYN         = `START_SYN        ,
+  parameter START_CNT         = `START_CNT        
+ )
+ (
+  //output  [3:0] o_normal_led               ,
+  output        o_ber_ok_led_I ,
+  output        o_ber_ok_led_Q ,
+  
+  //input  [                 3:0]  i_sw        ,
+  input         i_reset        ,
+  input         clk
+ );
+  
+  
+  //////////////// Internal wires
+  wire                             w_reset                   ;
+  wire [                      1:0] w_control_phase_select    ;
+  wire                             w_control_poly_filt       ;
+  wire                             w_control_for_rate_2      ;
+  wire                             w_control_for_rate_1      ;
+  wire                             w_save_fse_shifters       ;
+  wire                             w_tx_prbsI_bit_to_tx_filtI;
+  wire                             w_tx_prbsQ_bit_to_tx_filtQ;
+  wire signed [NBT_TXFILT_OUT-1:0] w_tx_filtI_to_noiseI      ;
+  wire signed [NBT_TXFILT_OUT-1:0] w_tx_filtQ_to_noiseQ      ;
+  wire signed [     NBT_NOISE-1:0] w_noiseI_to_add           ;
+  wire signed [     NBT_NOISE-1:0] w_noiseQ_to_add           ;
+  wire signed [ NBT_NOISY_SYM-1:0] w_ch_nsigI_to_chfiltI     ;
+  wire signed [ NBT_NOISY_SYM-1:0] w_ch_nsigQ_to_chfiltQ     ;
+  wire signed [NBT_CHFILT_OUT-1:0] w_ch_filtI_to_aa_filtI    ;
+  wire signed [NBT_CHFILT_OUT-1:0] w_ch_filtQ_to_aa_filtQ    ;
+  wire signed [NBT_AAFILT_OUT-1:0] w_rx_aa_filtI_to_dwr2I    ;
+  wire signed [NBT_AAFILT_OUT-1:0] w_rx_aa_filtQ_to_dwr2Q    ;
+  wire signed [NBT_AAFILT_OUT-1:0] w_rx_dwr2I_to_fse         ;
+  wire signed [NBT_AAFILT_OUT-1:0] w_rx_dwr2Q_to_fse         ;
+  wire signed [   NBT_FSE_OUT-1:0] w_rx_symI_to_demapI       ;
+  wire signed [   NBT_FSE_OUT-1:0] w_rx_symQ_to_demapQ       ;
+  wire                             w_rx_bitI_to_ber          ;
+  wire                             w_rx_bitQ_to_ber          ;
+  wire                             w_ber_ok_led_I            ;
+  wire                             w_ber_ok_led_Q            ;
+  
+
+  ////////////////  Convert reset signal from active-low to active-high
+  assign w_reset  = ~i_reset;
+  
+
+  //////////////// Generate control signals for differentes rates
+  control
+    u_control (
+      .o_counter          (w_control_phase_select),
+      .o_count_max        (w_control_poly_filt   ),
+      .o_count_half_or_max(w_control_for_rate_2  ),
+      .o_count_max_rate1  (w_control_for_rate_1  ),
+      .o_save_fse_shifters(w_save_fse_shifters   ),
+      .i_reset            (w_reset               ),
+      .clk                (clk                   )
+    );
+  
+  
+
+  ///////////////////////////////////////////////////////////////////////////
+  //                              TRANSMITTER                              //
+  ///////////////////////////////////////////////////////////////////////////
+
+  //////////////// PRBS generator for I and Q channels
+  prbs9 #(
+    .SEED(PRBS_SEED_I)
+  ) u_tx_prbs9_I (
+    .o_new_bit(w_tx_prbsI_bit_to_tx_filtI),
+    .i_ctrl   (w_control_for_rate_1      ),
+    .i_reset  (w_reset                   ),  
+    .clk      (clk                       )            
+  );
+  
+  prbs9#(
+    .SEED(PRBS_SEED_Q)
+  ) u_tx_prbs9_Q (
+    .o_new_bit(w_tx_prbsQ_bit_to_tx_filtQ),
+    .i_ctrl   (w_control_for_rate_1      ),
+    .i_reset  (w_reset                   ),  
+    .clk      (clk                       )            
+  );
+
+
+  //////////////// Polyphase filters for interpolation and shaping  
+  polyph_filter #(
+    .FILE_COEFF(TXFILT_COEFF_FILE),
+    .NBT_COEF  (NBT_TXFILT_COEF  ),
+    .NBF_COEF  (NBF_TXFILT_COEF  ),
+    .NBT_OUT   (NBT_TXFILT_OUT   ),
+    .NBF_OUT   (NBF_TXFILT_OUT   ),
+    .NBAUD     (NBAUD            ),
+    .OS        (OVERSAMP         )
+  ) u_tx_filter_I (
+    .o_os_data           (w_tx_filtI_to_noiseI      ),
+    .i_is_data           (w_tx_prbsI_bit_to_tx_filtI),
+    .i_phase_num_of_coeff(w_control_phase_select    ),
+    .i_ctrl              (w_control_poly_filt       ),
+    .i_reset             (w_reset                   ),            
+    .clk                 (clk                       )
+  );
+  
+  polyph_filter #(
+    .FILE_COEFF(TXFILT_COEFF_FILE),
+    .NBT_COEF  (NBT_TXFILT_COEF  ),
+    .NBF_COEF  (NBF_TXFILT_COEF  ),
+    .NBT_OUT   (NBT_TXFILT_OUT   ),
+    .NBF_OUT   (NBF_TXFILT_OUT   ),
+    .NBAUD     (NBAUD            ),
+    .OS        (OVERSAMP         )
+  ) u_tx_filter_Q (
+    .o_os_data           (w_tx_filtQ_to_noiseQ      ),
+    .i_is_data           (w_tx_prbsQ_bit_to_tx_filtQ),
+    .i_phase_num_of_coeff(w_control_phase_select    ),
+    .i_ctrl              (w_control_poly_filt       ),
+    .i_reset             (w_reset                   ),            
+    .clk                 (clk                       )
+  );
+  
+  
+
+  ///////////////////////////////////////////////////////////////////////////
+  //                                CHANNEL                                //
+  ///////////////////////////////////////////////////////////////////////////
+
+   //////////////// Generate and add Gaussian noise to the transmitted signal
+  // Generate noise
+  gng_top #(
+    .Z1_I     (NOISE_SEED1_I), 
+    .Z2_I     (NOISE_SEED2_I), 
+    .Z3_I     (NOISE_SEED3_I), 
+    .Z1_Q     (NOISE_SEED1_Q), 
+    .Z2_Q     (NOISE_SEED2_Q), 
+    .Z3_Q     (NOISE_SEED3_Q), 
+    .SIGMA    (SIGMA        ), 
+    .NBT_SIGMA(NBT_SIGMA    ), 
+    .NBF_SIGMA(NBF_SIGMA    ), 
+    .NBT_NOISE(NBT_NOISE    ), 
+    .NBF_NOISE(NBF_NOISE    ) 
+  ) u_ch_gng_top (
+    .o_noise_I(w_noiseI_to_add),
+    .o_noise_Q(w_noiseQ_to_add),
+    .i_reset  (i_reset        ),
+    .clk      (clk            )
+  );
+  
+  // Add noise to signal
+  assign w_ch_nsigI_to_chfiltI = w_tx_filtI_to_noiseI + w_noiseI_to_add;
+  assign w_ch_nsigQ_to_chfiltQ = w_tx_filtQ_to_noiseQ + w_noiseQ_to_add;
+
+
+  //////////////// FIR Channel filters to model ISI
+  fir_filter #(
+    .NUM_COEFF (NUM_CHFILT_COEF  ), 
+    .FILE_COEFF(CHFILT_COEFF_FILE),
+    .NBT_IN    (NBT_NOISY_SYM    ), 
+    .NBF_IN    (NBF_NOISY_SYM    ), 
+    .NBT_COEFF (NBT_CHFILT_COEF  ),
+    .NBF_COEFF (NBF_CHFILT_COEF  ), 
+    .NBT_OUT   (NBT_CHFILT_OUT   ), 
+    .NBF_OUT   (NBF_CHFILT_OUT   ) 
+  ) u_ch_filt_I (
+    .o_os_data(w_ch_filtI_to_aa_filtI),
+    .i_is_data(w_ch_nsigI_to_chfiltI ),
+    .i_reset  (w_reset               ),
+    .clk      (clk                   ) 
+  );
+
+  fir_filter #(
+    .NUM_COEFF (NUM_CHFILT_COEF  ), 
+    .FILE_COEFF(CHFILT_COEFF_FILE),
+    .NBT_IN    (NBT_NOISY_SYM    ), 
+    .NBF_IN    (NBF_NOISY_SYM    ), 
+    .NBT_COEFF (NBT_CHFILT_COEF  ),
+    .NBF_COEFF (NBF_CHFILT_COEF  ), 
+    .NBT_OUT   (NBT_CHFILT_OUT   ), 
+    .NBF_OUT   (NBF_CHFILT_OUT   ) 
+  ) u_ch_filt_Q (
+    .o_os_data(w_ch_filtQ_to_aa_filtQ),
+    .i_is_data(w_ch_nsigQ_to_chfiltQ ),
+    .i_reset  (w_reset               ),
+    .clk      (clk                   ) 
+  );
+
+
+
+  ///////////////////////////////////////////////////////////////////////////
+  //                                RECEIVER                               //
+  ///////////////////////////////////////////////////////////////////////////
+
+  //////////////// FIR filters to reduce Aliasing Effects
+  fir_filter #(
+    .NUM_COEFF (NUM_AAFILT_COEF  ), 
+    .FILE_COEFF(AAFILT_COEFF_FILE),
+    .NBT_IN    (NBT_CHFILT_OUT   ), 
+    .NBF_IN    (NBF_CHFILT_OUT   ), 
+    .NBT_COEFF (NBT_AAFILT_COEF  ),
+    .NBF_COEFF (NBF_AAFILT_COEF  ), 
+    .NBT_OUT   (NBT_AAFILT_OUT   ), 
+    .NBF_OUT   (NBF_AAFILT_OUT   ) 
+  ) u_aa_filt_I (
+    .o_os_data(w_rx_aa_filtI_to_dwr2I),
+    .i_is_data(w_ch_filtI_to_aa_filtI),
+    .i_reset  (w_reset               ),
+    .clk      (clk                   ) 
+  );
+
+  fir_filter #(
+    .NUM_COEFF (NUM_AAFILT_COEF  ), 
+    .FILE_COEFF(AAFILT_COEFF_FILE),
+    .NBT_IN    (NBT_CHFILT_OUT   ), 
+    .NBF_IN    (NBF_CHFILT_OUT   ), 
+    .NBT_COEFF (NBT_AAFILT_COEF  ),
+    .NBF_COEFF (NBF_AAFILT_COEF  ), 
+    .NBT_OUT   (NBT_AAFILT_OUT   ), 
+    .NBF_OUT   (NBF_AAFILT_OUT   ) 
+  ) u_aa_filt_Q (
+    .o_os_data(w_rx_aa_filtQ_to_dwr2Q),
+    .i_is_data(w_ch_filtQ_to_aa_filtQ),
+    .i_reset  (w_reset               ),
+    .clk      (clk                   ) 
+  );
+
+
+  //////////////// Downsample the received signal by a factor of 2
+  downsamp #(
+    .NBT_IN_OUT(NBT_AAFILT_OUT), 
+    .NBF_IN_OUT(NBT_AAFILT_OUT)
+  ) u_dwsamp_r2_I (
+    .o_os_data(w_rx_dwr2I_to_fse     ),
+    .i_is_data(w_rx_aa_filtI_to_dwr2I), 
+    .i_en     (w_control_for_rate_2  ),
+    .i_reset  (w_reset               ),
+    .clk      (clk                   ) 
+  );
+
+  downsamp #(
+    .NBT_IN_OUT (NBT_AAFILT_OUT), 
+    .NBF_IN_OUT (NBT_AAFILT_OUT)
+  ) u_dwsamp_r2_Q (
+    .o_os_data(w_rx_dwr2Q_to_fse     ),
+    .i_is_data(w_rx_aa_filtQ_to_dwr2Q),
+    .i_en     (w_control_for_rate_2  ),
+    .i_reset  (w_reset               ),
+    .clk      (clk                   ) 
+  );
+
+
+  //////////////// Adaptive Filter: FSE with LMS adaptation
+  adaptive_filter #(
+    .STEP    (LMS_STEP      ),
+    .LEAK    (LMS_LEAK      ),
+    .NBT_STEP(NBT_LMS_STEP  ),
+    .NBF_STEP(NBF_LMS_STEP  ),
+    .NBT_LEAK(NBT_LMS_LEAK  ),
+    .NBF_LEAK(NBF_LMS_LEAK  ),
+    .NUM_TAPS(NUM_FSE_TAPS  ),
+    .NBT_IN  (NBT_AAFILT_OUT),
+    .NBF_IN  (NBF_AAFILT_OUT),
+    .NBT_TAPS(NBT_FSE_TAPS  ),
+    .NBF_TAPS(NBF_FSE_TAPS  ),
+    .NBT_OUT (NBT_FSE_OUT   ),
+    .NBF_OUT (NBF_FSE_OUT   ),
+    .NBT_ERR (NBT_TAPS_ERR  ),
+    .NBF_ERR (NBF_TAPS_ERR  )
+  ) u_adaptive_filter (
+    .o_os_data_I (w_rx_symI_to_demapI ),
+    .o_os_data_Q (w_rx_symQ_to_demapQ ),
+    .i_is_data_I (w_rx_dwr2I_to_fse   ),
+    .i_is_data_Q (w_rx_dwr2Q_to_fse   ),
+    .i_en_rate2  (w_control_for_rate_2),
+    .i_en_rate1  (w_control_for_rate_1),
+    .i_save_shtrs(w_save_fse_shifters ),
+    .i_reset     (w_reset             ),
+    .clk         (clk                 )
+  );
+
+
+  //////////////// Demap bits from received QPSK symbols 
+  assign w_rx_bitI_to_ber = w_rx_symI_to_demapI[NBT_FSE_OUT-1];
+  assign w_rx_bitQ_to_ber = w_rx_symQ_to_demapQ[NBT_FSE_OUT-1];
+
+
+  //////////////// BER Control measurement and validation
+  ber #(
+    .PRBS_SEED_I    (PRBS_SEED_I    ),
+    .PRBS_SEED_Q    (PRBS_SEED_Q    ),
+    .PRBS_MAX_CYCLES(PRBS_MAX_CYCLES),
+    .START_SYN      (START_SYN      ),
+    .START_CNT      (START_CNT      )
+  ) u_ber_IjQ (
+    .o_ber_ok_led_I(w_ber_ok_led_I      ),
+    .o_ber_ok_led_Q(w_ber_ok_led_Q      ),
+    .i_rx_bit_I    (w_rx_bitI_to_ber    ),
+    .i_rx_bit_Q    (w_rx_bitQ_to_ber    ),
+    .i_en_rate1    (w_control_for_rate_1), 
+    .i_reset       (w_reset             ), 
+    .clk           (clk                 ) 
+  );
+
+
+  //////////////// Assign processed signals to output ports  
+  assign o_ber_ok_led_I = w_ber_ok_led_I;
+  assign o_ber_ok_led_Q = w_ber_ok_led_Q;
+
+
+endmodule
+
+
