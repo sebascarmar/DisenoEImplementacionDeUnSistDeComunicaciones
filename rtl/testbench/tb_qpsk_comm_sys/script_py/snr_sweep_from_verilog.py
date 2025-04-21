03d0b13fb2c1ecfa8f75971f227b438d60f27a6a
Sebastian Carreño Marin <sebacarmar@gmail.com>
2025-04-20 22:31:52 -0300

    Agrega script que traza curva de BER con datos de Verilog

---
+++ rtl/testbench/tb_qpsk_comm_sys/script_py/snr_sweep_from_verilog.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+snr    = np.array([7.184    , 8.168    , 9.279    , 10.107   , 11.022   , 12.045])
+bersI  = np.array([1.525e-02, 7.324e-03, 2.801e-03, 1.206e-03, 3.754e-04, 6.990e-05])
+bersQ  = np.array([1.549e-02, 7.412e-03, 2.829e-03, 1.250e-03, 3.987e-04, 9.061e-05])
+th_ber = np.array([1.111e-02, 5.217e-03, 1.805e-03, 6.840e-04, 1.875e-04, 3.145e-05])
+
+
+# Plot
+plt.figure(figsize=[14,6])
+plt.title('BER vs SNR - (Results from Verilog testbench)')
+plt.semilogy(snr, th_ber, 'r', linewidth=2.0)
+plt.semilogy(snr, bersI, 'b', linewidth=2.0)
+plt.semilogy(snr, bersQ, 'g', linewidth=2.0)
+plt.xlabel('SNR(dB)')
+plt.ylabel('BER')
+plt.grid(True)
+plt.ylim(1e-5,0.7e-1)
+plt.xlim(6.9,12.1)
+plt.legend(['SNR theo','SNR I','SNR Q'])
+plt.show()
