530639003c8e5c753f13e39c075d243dcf0e43b5
TuNombre <tuemail@example.com>
2025-05-02 22:24:25 -0300

    Agrega uBlaze sin clk wizar

---
+++ rtl/uBlaze/ip/uBlaze_clk_wiz_0_0/uBlaze_clk_wiz_0_0_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2023.1 (lin64) Build 3865809 Sun May  7 15:04:56 MDT 2023
+-- Date        : Mon Apr 28 23:19:38 2025
+-- Host        : daniel-lmint running 64-bit Linux Mint 20.3
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/danielito/Escritorio/CorreccionDeEfectosDeCanal/rtl/build/tesis_beta/Modulos/uBlaze/ip/uBlaze_clk_wiz_0_0/uBlaze_clk_wiz_0_0_stub.vhdl
+-- Design      : uBlaze_clk_wiz_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35ticsg324-1L
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity uBlaze_clk_wiz_0_0 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    resetn : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end uBlaze_clk_wiz_0_0;
+
+architecture stub of uBlaze_clk_wiz_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,resetn,locked,clk_in1";
+begin
+end;
