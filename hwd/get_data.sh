abd90c0a60a0c801b7ee298b516ef7da0fbda471
TuNombre <tuemail@example.com>
2025-05-15 23:54:21 -0300

    Script de bash para traer datos log.

---
+++ hwd/get_data.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+
+# Para que este scip de bash funcione se debe generar una clave ssh publica y copiarla al servidor 
+#
+# 1) coloca en la consola ssh-keygen -t rsa -b 4096 -C "user@fulgorip1.hopto.org"
+# 2) luego se pedira Enter file in which to save the key (/home/tu_usuario/.ssh/id_rsa): 
+# 3) la contraseña es opcional, sino no coloques nada preciona enter 3 veces para que nunca pida contraseña y se genere el SHA 
+# 4) Copia la clave publica al servidor ssh-copy-id -p 4466 user@fulgorip1.hopto.org
+# 5) Prueba conexion ssh -p 4466 user@fulgorip1.hopto.org
+#
+# NOTA: Se debe instalar scp sudo apt install openssh-client
+
+# Dowload data in sweep_snr
+for x in {7..17}; do
+    echo "Download sweep_with_snr_$x.txt..."
+    scp -P 4466 user@fulgorip1.hopto.org:/home/user/work_dda/djuarez/scripts/sweep_with_snr_${x}.txt ~/Escritorio/Barrido_snr_hw/
+done
+
+# Download data stored 
+for x in {7..17}; do
+    echo "Download sweep_with_snr_$x.txt..."
+    scp -P 4466 user@fulgorip1.hopto.org:/home/user/work_dda/djuarez/scripts/file_rx_symI_dwr2_snr_${x}.txt ~/Escritorio/Barrido_snr_hw/snr${x}/  
+    scp -P 4466 user@fulgorip1.hopto.org:/home/user/work_dda/djuarez/scripts/file_rx_symQ_dwr2_snr_${x}.txt ~/Escritorio/Barrido_snr_hw/snr${x}/  
+    scp -P 4466 user@fulgorip1.hopto.org:/home/user/work_dda/djuarez/scripts/file_rx_symI_dwr1_snr_${x}.txt ~/Escritorio/Barrido_snr_hw/snr${x}/  
+    scp -P 4466 user@fulgorip1.hopto.org:/home/user/work_dda/djuarez/scripts/file_rx_symQ_dwr1_snr_${x}.txt ~/Escritorio/Barrido_snr_hw/snr${x}/  
+    scp -P 4466 user@fulgorip1.hopto.org:/home/user/work_dda/djuarez/scripts/file_fse_taps_I_snr_${x}.txt ~/Escritorio/Barrido_snr_hw/snr${x}/ 
+    scp -P 4466 user@fulgorip1.hopto.org:/home/user/work_dda/djuarez/scripts/file_fse_taps_Q_snr_${x}.txt ~/Escritorio/Barrido_snr_hw/snr${x}/   
+done
+
+
